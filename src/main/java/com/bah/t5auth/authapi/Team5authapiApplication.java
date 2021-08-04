ac410bc04ec526728a04aca9e7989253d82cf28d
user <user@this.com>
2021-08-04 15:00:43 -0400

    First commit

---
+++ src/main/java/com/bah/t5auth/authapi/Team5authapiApplication.java
@@ -0,0 +1,13 @@
+package com.bah.t5auth.authapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5authapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5authapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bah/t5auth/authapi/Team5authapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bah.t5auth.authapi;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Team5authapiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
